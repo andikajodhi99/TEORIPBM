001ca9a4ef9540845e415630ad488cf3c332493f
andikajodhi99 <andikajodhi@gmail.com>
2019-11-23 11:11:50 +0700

    23November2019

---
+++ index.php
@@ -0,0 +1,8 @@
+
+<?php
+ session_start();
+ if(!isset($_SESSION["status"]))
+ 	echo "kontol";
+ else
+	echo "memek";
+?>
---
+++ proseslogin.php
@@ -0,0 +1,16 @@
+<?php 
+include 'config.php';
+session_start();
+$username = $_POST['username'];
+$password = $_POST['password'];
+ 
+$login = mysqli_query($db, "select * from admin where username='$username' and password='$password'");
+$cek = mysqli_num_rows($login);
+if($cek > 0){
+	$_SESSION['username'] = $username;
+    $_SESSION['status'] = "login";
+	header("location:menu.html");
+}else{
+    header("location:login.html");	
+}
+?>
